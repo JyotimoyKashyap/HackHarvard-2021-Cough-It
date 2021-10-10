8d3bd519fd744bcad77f710b745192008fd163ce
Gaurav Das <gauravdas014@gmail.com>
2021-10-10 15:40:17 +0530

    feat: add api to call model

--- API/controllers/controller.js
+++ API/controllers/controller.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 const { PythonShell } = require("python-shell");
-const { spawn } = require("child_process");
-const Mp32Wav = require("mp3-to-wav");
-const ffmpeg = require("ffmpeg");
-const shell = require("shelljs");
 const { exec } = require("child_process");
+const axios = require("axios");
 
 exports.getPrediction = async (req, res) => {
   try {
@@ -18,30 +15,9 @@ exports.getPrediction = async (req, res) => {
       )
     );
 
-    // try {
-    //   const mp32Wav = new Mp32Wav(
-    //     "./assets/data/sample.mp3",
-    //     "./assets/output"
-    //   );
-    //   mp32Wav.exec();
-    // } catch (err) {
-    //   console.log(err);
-    // }
-
     let dataToSend;
 
     let options = {};
-
-    // await PythonShell.run(
-    //   "scripts/convertToWav.py",
-    //   options,
-    //   function (err, result) {
-    //     if (err) throw err;
-    //     console.log(result.toString());
-    //   }
-    // );
-
-    // shell.exec("scripts/conversion.sh");
 
     exec(
       "ffmpeg -i assets/data/sample.m4a assets/output/sample.wav",
@@ -50,30 +26,47 @@ exports.getPrediction = async (req, res) => {
           //some err occurred
           console.error(err);
         } else {
-          // the *entire* stdout and stderr (buffered)
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
+          console.log("Converted");
         }
       }
     );
 
-    await PythonShell.run(
+    PythonShell.run(
       "scripts/preprocessing.py",
       options,
-      function (err, result) {
+      async function (err, result) {
         if (err) throw err;
         dataToSend = result.toString();
-        return res.status(200).json({
-          status: "success",
-          data: dataToSend,
-        });
+
+        const data = JSON.stringify(dataToSend);
+
+        const config = {
+          method: "post",
+          url: "https://runtime.sagemaker.us-east-2.amazonaws.com/endpoints/tensorflow-inference-2021-10-10-02-56-05-523/invocations",
+          headers: {
+            "X-Amz-Content-Sha256":
+              "beaead3198f7da1e70d03ab969765e0821b24fc913697e929e726aeaebf0eba3",
+            "X-Amz-Date": "20211010T095700Z",
+            Authorization:
+              "AWS4-HMAC-SHA256 Credential=AKIAXV27PUM6AN7L5KGU/20211010/us-east-2/sagemaker/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=9e0dc83ba031fd9d19112c396329799d83b1fa5f83836bb18463a1366e989bd0",
+            "Content-Type": "application/json",
+          },
+          data: data,
+        };
+
+        await axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            return res.status(200).json({
+              status: "success",
+              data: response.data,
+            });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       }
     );
-
-    // return res.status(200).json({
-    //   status: "success",
-    //   // data: dataToSend,
-    // });
   } catch (err) {
     console.log(err);
     res.status(400).json({
